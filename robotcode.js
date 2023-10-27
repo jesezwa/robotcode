35d118d3fc132073d22c158854dce3621cde7c45
jesezwa <145676322+jesezwa@users.noreply.github.com>
2023-10-27 10:46:33 +0200

    Comments toegevoegd

--- robotcode.js
+++ robotcode.js
@@ -1,62 +1,83 @@
-// variabele voor bijhouden tijd en veranderen van
-
+// Variabele voor het instellen van de timerduur voor het activeren van de lichten (in milliseconden), hiermee bepalen we ook de totale tijd van de timer
 let lightTimer = 500;
+// Variabele voor het tellen en onthouden hoever we zijn
 let timeCounter = 0;
-// let i = 0; 
 
-
-// functie die lampjes laat branden
+// Functie om de lichten te activeren
 function timer() {
+    // Loopt door de lichten van 0 tot 10
     for (let i = 0; i <= 10; i++) {
+        // Pauzeer voor 'lightTimer' milliseconden voordat het volgende licht wordt geactiveerd
         pause(lightTimer);
+        // Zet het i licht op rood
         light.setPixelColor(i, Colors.Red);
+        // Verhoog de tijdsteller
         timeCounter++;
+        // Log de tijdsteller naar de console
         console.log(timeCounter);
     }
-    waitingTime()
+    // Roep de functie aan om de wachttijd in te schakelen
+    waitingTime();
 }
 
-timer()
 
 
+// Functie om te wachten nadat alle lichten zijn geactiveerd
 function waitingTime() {
+    // Controleer of de tijdsteller 11 is
     if (timeCounter === 11) {
         console.log("tijd");
+        // Maak alle lichten leeg
         light.clear();
+        // Reset de tijdsteller
         timeCounter = 0;
-        light.setBrightness(0)
-        light.setAll(Colors.White)
+        // Zet de helderheid van alle lichten op 0 en verander de kleur naar wit
+        light.setBrightness(0);
+        light.setAll(Colors.White);
+        // Loop door de helderheidsniveaus 
         for (let i = 240; i < 256; i++) {
-            pause(117)
+            // Pauzeer voor 117 milliseconden
+            pause(117);
+            // Zet de helderheid van alle lichten naar het huidige niveau (i)
             light.setBrightness(i);
-            console.log(i)
+            console.log(i);
+            // Verhoog de tijdsteller
             timeCounter++;
 
+            // Controleer of knop A is ingedrukt
             if (input.buttonA.isPressed()) {
-                light.clear()
+                // Maak alle lichten leeg
+                light.clear();
+                // Reset de tijdsteller en de loopvariabele (i)
                 i = 256;
                 timeCounter = 0;
-                timer()
+                // Roep de timerfunctie opnieuw aan om het programma te herstarten
+                timer();
             }
-
         }
-    } toLate()
+    }
+    // Roep de functie om aan te geven dat er te laat is gereageerd
+    toLate();
 }
 
-
+// Functie om te laten zien dat er te laat is gereageerd
 function toLate() {
-    music.baDing.loop()
+    // Speel een geluid af (continu) wanneer er te laat is gereageerd
+    music.baDing.loop();
 
-   input.buttonA.onEvent(ButtonEvent.Click, function () {
-       
-           console.log('if')
-           light.clear()
-           timeCounter = 0;
-           music.stopAllSounds()
-           timer()
-      
-   })
-
+    // Voer de functie uit wanneer knop A wordt ingedrukt
+    input.buttonA.onEvent(ButtonEvent.Click, function () {
+        // Maak alle lichten leeg
+        light.clear();
+        // Reset de tijdsteller
+        timeCounter = 0;
+        // Stop met het afspelen van alle geluiden
+        music.stopAllSounds();
+        // Roep de timerfunctie opnieuw aan om het programma te herstarten
+        timer();
+    });
 }
 
+// Roep de timerfunctie aan om het programma te starten
+timer();
 
