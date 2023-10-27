65c81366681f1863b6f36396f2e6cfd770af8696
jesezwa <145676322+jesezwa@users.noreply.github.com>
2023-10-27 10:03:47 +0200

    Code versimpeld

--- robotcode.js
+++ robotcode.js
@@ -1,75 +1,56 @@
-let addTime = 500;
+// variabele voor bijhouden tijd en veranderen van
 
-function adhdWatch() {
-  buttonClicked = false;
+let lightTimer = 500;
+let timeCounter = 0;
+// let i = 0; 
 
-  for (let i = 0; i < 10; i++) {
-    pause(addTime);
 
-    light.setPixelColor(i, Colors.Red);
-
-    console.log(i);
-  }
-
-  light.setBrightness(0);
-
-  // Vloeiend worden de lichtjes feller over een periode van 60 seconden
-
-  for (let i = 0; i < 256; i++) {
-    // Lampjes gaan aan
-
-    light.setAll(Colors.White);
-
-    pause(117);
-
-    light.setBrightness(i);
-
-    console.log(i);
-
-    // Controleer of er op knop A is gedrukt
-
-    if (input.buttonA.wasPressed()) {
-      light.clear(); // Schakel de lampjes uit
-
-      return; // Stop de functie als er op knop A wordt gedrukt
-    }
-  }
-}
-// aan passing
+// functie die lampjes laat branden
 function timer() {
-  for (let i = 0; i < 10; i += 2) {
-    pause(5000);
-
-    light.setPixelColor(i, Colors.Red);
-
-    console.log(i);
-  }
+    for (let i = 0; i <= 10; i++) {
+        pause(lightTimer);
+        light.setPixelColor(i, Colors.Red);
+        timeCounter++;
+        console.log(timeCounter);
+    }
+    waitingTime()
 }
 
-let buttonClicked = false;
+timer()
 
-// Wanneer klikt binnen 60 seconden, lampjes weer uit, code herhaalt
 
-input.buttonA.onEvent(ButtonEvent.Click, function () {
-  if (!buttonClicked) {
-    buttonClicked = true;
+function waitingTime() {
+    if (timeCounter === 11) {
+        console.log("tijd");
+        light.clear();
+        timeCounter = 0;
+        light.setBrightness(0)
+        light.setAll(Colors.White)
+        for (let i = 240; i < 256; i++) {
+            pause(117)
+            light.setBrightness(i);
+            console.log(i)
+            timeCounter++;
 
-    light.clear(); // Schakel de lampjes uit
+            if (input.buttonA.isPressed()) {
+                light.clear()
+                i = 256;
+                timeCounter = 0;
+                timer()
+            }
 
-    adhdWatch(); // Herstart de code
-  }
-});
-
-input.buttonB.onEvent(ButtonEvent.Click, function () {
-  addTime += 500;
-});
-
-// Anders maak irritant geluid tot er wordt geklikt
-
-if (!buttonClicked) {
-  music.playTone(262, music.beat(BeatFraction.Whole)); // Voorbeeldgeluid (C)
-
-  // Voeg hier code toe om het irritante geluid te herhalen totdat er wordt geklikt
+        }
+    } toLate()
 }
 
-adhdWatch(); // Start de code
+function toLate() {
+    music.baDing.loop()
+
+    if (input.buttonA.isPressed()) {
+        light.clear()
+        timeCounter = 0;
+        music.stopAllSounds()
+        timer()
+    }
+
+}
