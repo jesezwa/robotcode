6e90191fc34fccf32839353bf34a5f50d7c67e65
jesezwa <145676322+jesezwa@users.noreply.github.com>
2023-10-31 09:48:20 +0100

    Quackmate V3
    Bug in wachttijd

--- robotcode.js
+++ robotcode.js
@@ -1,24 +1,28 @@
 // Variabele voor het instellen van de timerduur voor het activeren van de lichten (in milliseconden), hiermee bepalen we ook de totale tijd van de timer
-const startTime = 500;
-let lightTimer = 500;
+const startTime = 2727;
+let lightTimer = 2727;
+// Variabele voor het tellen en onthouden hoever we zijn
 let timeCounter = 0;
-const maxTime = 10000; // Maximale waarde voor lightTimer
-const addTime = 500; // De hoeveelheid tijd toegevoegd bij het klikken op knop B
+const addTime = 2727;
+const maxTime = addTime * 3;
+
 
 // Functie om de lichten te activeren
 function timer() {
-    // Event handlers voor knop B om de timerduur aan te passen
+
+    music.stopAllSounds();
     input.buttonB.onEvent(ButtonEvent.Click, function () {
         lightTimer += addTime;
-        console.log(lightTimer);
+        console.log(maxTime)
+        console.log(lightTimer)
         if (lightTimer >= maxTime) {
-            lightTimer = maxTime; // Beperk lightTimer tot maxTime
+            lightTimer = maxTime
         }
-    });
+    })
 
     input.buttonB.onEvent(ButtonEvent.LongClick, function () {
-        lightTimer = startTime; // Reset lightTimer naar de oorspronkelijke waarde
-    });
+        lightTimer = startTime;
+    })
 
     // Loopt door de lichten van 0 tot 10
     for (let i = 0; i <= 10; i++) {
@@ -34,38 +38,46 @@ function timer() {
     // Roep de functie aan om de wachttijd in te schakelen
     waitingTime();
 }
-
-
-
 // Functie om te wachten nadat alle lichten zijn geactiveerd
 function waitingTime() {
     // Controleer of de tijdsteller 11 is
     if (timeCounter === 11) {
+        music.baDing.play()
         console.log("tijd");
+
         // Maak alle lichten leeg
         light.clear();
+
         // Reset de tijdsteller
         timeCounter = 0;
+
         // Zet de helderheid van alle lichten op 0 en verander de kleur naar wit
         light.setBrightness(0);
         light.setAll(Colors.White);
+
         // Loop door de helderheidsniveaus 
-        for (let i = 240; i < 256; i++) {
+        for (let i = 0; i < 256; i++) {
+
             // Pauzeer voor 117 milliseconden
             pause(117);
+
             // Zet de helderheid van alle lichten naar het huidige niveau (i)
             light.setBrightness(i);
             console.log(i);
+
             // Verhoog de tijdsteller
             timeCounter++;
 
             // Controleer of knop A is ingedrukt
-            if (input.buttonA.isPressed()) {
+            if (input.touchA2.isPressed() || input.touchA3.isPressed()) {
+
                 // Maak alle lichten leeg
                 light.clear();
-                // Reset de tijdsteller en de loopvariabele (i)
+
+                // Reset de tijdsteller en de loopvariabele (i) naar 256, zodat for stopt
                 i = 256;
                 timeCounter = 0;
+
                 // Roep de timerfunctie opnieuw aan om het programma te herstarten
                 timer();
             }
@@ -74,24 +86,47 @@ function waitingTime() {
     // Roep de functie om aan te geven dat er te laat is gereageerd
     toLate();
 }
-
 // Functie om te laten zien dat er te laat is gereageerd
 function toLate() {
+
     // Speel een geluid af (continu) wanneer er te laat is gereageerd
     music.baDing.loop();
 
+
     // Voer de functie uit wanneer knop A wordt ingedrukt
-    input.buttonA.onEvent(ButtonEvent.Click, function () {
+    input.touchA2.onEvent(ButtonEvent.Click, function () {
+
+
         // Maak alle lichten leeg
         light.clear();
+
         // Reset de tijdsteller
         timeCounter = 0;
+
+        // Stop met het afspelen van alle geluiden
+
+
+        // Roep de timerfunctie opnieuw aan om het programma te herstarten
+        timer();
+    });
+
+    input.touchA3.onEvent(ButtonEvent.Click, function () {
+
+        // Maak alle lichten leeg
+        light.clear();
+
+        // Reset de tijdsteller
+        timeCounter = 0;
+
         // Stop met het afspelen van alle geluiden
         music.stopAllSounds();
+
         // Roep de timerfunctie opnieuw aan om het programma te herstarten
         timer();
     });
 }
+// Roep de timerfunctie aan om het programma te starten
+timer();
 
 // Roep de timerfunctie aan om het programma te starten
 timer();
